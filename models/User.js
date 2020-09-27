ce0edd6daa9125093b501ef66eb6dbf7be37f800
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-09-27 13:39:24 +0300

    Расписал схему Пользователя. Провалидировал поля email, passwod, name. Password возвращаю в хешированном виде

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator: (value) => /\w/.test(value)
+    },
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (value) => isEmail(value),
+      message: 'Почта должна содержать минимум 2 символа и соответствовать шаблону example@email.com'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 2,
+    select: false,  //TODO: посомтреть что это значит
+  }
+});
+
+// TODO: findUserByCredentials - вернуться к конспектам и ознакомиться с этим методом.
+userSchema.findUserByCredentials = function(email, password) {
+  return finOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль.'))
+      }
+      return bcrypt.compare(password, user.password) //TODO: что это значит
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль.'))
+          }
+          return user;
+        })
+    })
+}
+
+module.exports = mongoose.model('user', userSchema);
