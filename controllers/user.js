3138e86b5641761cd36c26c8c1b9a6ebfa9923ec
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-09-27 22:29:18 +0300

    контроллер регистрации с хэшированным паролем

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const NotFoundError = require('../utils/NotFoundError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.getUser = (req, res, next) => {
   User.findById(req.params.id )
     .then((user) => {
@@ -27,9 +29,37 @@ module.exports.login = (req, res, next) => {
       if(!user) {
         throw new NotFoundError('Нет пользователя с таким id');
       }
+      const token = jwt.sign(
+        { _id: user.id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      );
+      res
+        .coockie('jwt', token, {
+          maxAge: 360000,
+          httpOnly: true,
+        })
+        .end()
     })
+    .catch(next);
 };
 
 module.exports.createUser = () => {
-
+  User.finOne({ email: req.body.email })
+    .then((hash) => {
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash,
+      })
+        .then((user) => {
+          res.status(201).send({
+            data: {
+              _id: user._id,
+              name: user.name,
+              email: user.email,
+            }
+          })
+        })
+    })
+    .catch(next);
 };
