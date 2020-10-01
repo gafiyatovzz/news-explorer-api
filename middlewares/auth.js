15536eba68292453d55084d141df4893e17b053d
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-10-01 20:46:49 +0300

    Правка авторизации. В токене была прописана заглушка, которая непозволяла пройти авторизацию.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,32 +1,31 @@
 const jwt = require('jsonwebtoken');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  // Если в заголовке header нет токена возвращаем ошибку со статусом
-  // 401 вместе с сообщением
-  if (!authorization) {
-  console.log('auth', authorization);
 
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' })
-  }
-
-  // Токен в заголовке найден, очищаем его от Bearer и верифицируем
-  const token = authorization.replace('Bearer ', '');
-  console.log('token', token);
-  let payload;
-  try {
-    payload = jwt.verify(token, 'super-strong-secret');
-console.log('payload', payload);
-
-  } catch (err) {
-console.log('err', err);
+  if (!authorization || authorization.startsWith('Bearer ')) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
-  // отправляем валидные данные
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production'
+        ? JWT_SECRET
+        : 'dev-secret',
+    );
+  } catch (err) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация' });
+  }
+
   req.user = payload;
-  next();
-}
+  return next();
+};
