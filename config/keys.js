efd6466c1eb361d1bb17e23a68adc947c4611449
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-10-09 11:38:37 +0300

    a

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 const prodKey = require('./keys.prod');
-const devKey = require('./keys.dev');
+// const devKey = require('./keys.dev');
 
 const URI = 'mongodb://localhost:27017/news-explorer-db';
 const PORT = 3000;
@@ -8,7 +8,6 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = prodKey;
 } else {
   module.exports = {
-    devKey,
     URI,
     PORT,
   };
