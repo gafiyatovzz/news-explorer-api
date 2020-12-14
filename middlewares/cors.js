46c5f5e98b172b49596f75d2c81727e6dc93c9b7
Zakir Gafiyatov <gafiyatovz@yandex.ru>
2020-12-14 12:41:57 +0000

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,7 @@
 
 module.exports = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
-}
+};
